62a64673ceed2fc9c480cac8c016855a989a6ec1
renegz <00077321@uca.edu.sv>
2024-03-09 23:18:15 -0600

    :memo: Some good comments

--- content-calendar/src/main/java/com/renegz/contentcalendar/repository/ContentCollectionRepository.java
+++ content-calendar/src/main/java/com/renegz/contentcalendar/repository/ContentCollectionRepository.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
+@Repository //Anotarlo como repositorio genera una instancia de esta clase
 public class ContentCollectionRepository {
 
     private final List<Content> contentList = new ArrayList<>();
